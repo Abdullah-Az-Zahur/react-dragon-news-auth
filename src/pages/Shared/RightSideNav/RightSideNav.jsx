b7e76742795203b1c8a3d5ddeab76c1285b05b1c
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-04-04 16:15:50 +0600

    Breaking News Slider Using React Marquee And Navbar

---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className="2xl">Right nav</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
